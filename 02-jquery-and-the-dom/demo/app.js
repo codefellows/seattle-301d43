9599efa778e5f6fdffcce9e164ce233502d7fb4a
mnfmnfm <mnfmnfm@gmail.com>
2018-09-04 14:40:19 -0700

    pass the linter

--- 02-jquery-and-the-dom/demo/app.js
+++ 02-jquery-and-the-dom/demo/app.js
@@ -1,36 +1,38 @@
+'use strict';
+/* global $ */
 //*****basic selectors*****//
 
 //element
-$('li')  //returns an array of all matching elements
+$('li'); //returns an array of all matching elements
 
 //class
-$('.form-element')
+$('.form-element');
 
 //id
-$('#beautiful-list')
+$('#beautiful-list');
 
 //parent descendent
-$('#list-container li')  //returns all <li>s anywhere inside the #list-container element
+$('#list-container li'); //returns all <li>s anywhere inside the #list-container element
 
 // parent > child
-$('#list-container > p')  //returns any <p> that is a DIRECT child of #list-container
+$('#list-container > p'); //returns any <p> that is a DIRECT child of #list-container
 
 //attribute
-$('input[name="textbox"]')
-$('li[data-category="list-item"]')
+$('input[name="textbox"]');
+$('li[data-category="list-item"]');
 
 //another way to do the previous selection
 var category = 'list-item';
-$('li[data-category="' + category + '"]')
+$('li[data-category="' + category + '"]');
 
 //get text of the matched element(s)
-$('h1').text()
+$('h1').text();
 
 //set text of the matched element(s)
-$('h1').text('this is some new text')
+$('h1').text('this is some new text');
 
 //set the data-category attribute of an li
-$('li').attr('data-category', 'list item')
+$('li').attr('data-category', 'list item');
 
 //get the data-category attribute of an li
 $('li').data();
@@ -38,12 +40,12 @@ $('li').data();
 //make a new <li> and append it to the <ul>
 var $newLi = $('.li-template').clone();
 $newLi.text('this is different text');
-$('ul').append($newLi)
+$('ul').append($newLi);
 
 //remove an element from the DOM
-$('button').remove()
+$('button').remove();
 
 //run a command as soon as the DOM loads
 $(document).ready(function() {
   alert('the DOM is ready now!');
-})
+});
