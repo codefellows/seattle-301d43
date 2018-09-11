d423d47fdc9288f99b85553565ccad7d058e8b3e
mnfmnfm <mnfmnfm@gmail.com>
2018-09-11 16:17:19 -0700

    add cors

--- 07-nodejs-npm-express/demo/server.js
+++ 07-nodejs-npm-express/demo/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 // to listen for and respond to a request:
 //   address/url
 //   method
